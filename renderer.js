1d07e10b379ba80b98fe3a98a3d133217632bba0
Mathew de Vin <mathewdevin03@gmail.com>
2022-06-07 15:56:50 -0700

    Document code

--- renderer.js
+++ renderer.js
@@ -1,24 +1,47 @@
 let audioPlaying = true;
 
+const buttonPlayPause = document.getElementById('audio-play-pause');
+const buttonNext = document.getElementById('audio-next');
+const buttonPrevious = document.getElementById('audio-previous');
+
+/**
+ * Call API to pause/play audio when the button is pressed
+ */
 const toggleAudioPlayPause = () => {
   if (audioPlaying) window.api.setAudioPause();
   else window.api.setAudioPlay();
   audioPlaying = !audioPlaying;
 }
+buttonPlayPause.addEventListener('click', toggleAudioPlayPause);
 
+/**
+ * Call API to skip to the next song with button is pressed
+ */
 const audioNext = () => {
   window.api.setAudioNext();
 }
+buttonNext.addEventListener('click', audioNext);
 
+/**
+ * Call API to go to previous song/beginning of song if button is pressed
+ */
 const audioPrevious = () => {
   window.api.setAudioPrevious();
 }
+buttonPrevious.addEventListener('click', audioPrevious);
 
-document.getElementById('audio-play-pause').addEventListener('click', toggleAudioPlayPause);
-document.getElementById('audio-next').addEventListener('click', audioNext);
-document.getElementById('audio-previous').addEventListener('click', audioPrevious);
-
+/**
+ * When the track is updated, change the title and artist
+ */
 window.api.onTrackUpdate((_, value) => {
   document.getElementById('track-title').innerText = value.Title;
   document.getElementById('track-artist').innerText = value.Artist;
+});
+
+/**
+ * When the status is updated outside the app, update the state
+ */
+window.api.onStatusUpdate((_, value) => {
+  // Statuses: "playing", "stopped", "paused", "forward-seek", "reverse-seek" or "error"
+  audioPlaying = value === 'playing';
 });
