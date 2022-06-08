0ccc924f459327363991333c3657889e2a159a28
Mathew de Vin <mathewdevin03@gmail.com>
2022-06-07 19:58:50 -0700

    Work with agents

--- src/bluezPlayer.js
+++ src/bluezPlayer.js
@@ -115,7 +115,7 @@ class BluezPlayer {
       const manager = await bus.getProxyObject('org.bluez', '/org/bluez');
       const managerInterface = manager.getInterface('org.bluez.AgentManager1');
       console.log(managerInterface);
-      managerInterface.RegisterAgent('/bluezplayer/agent', 'NoInputNoOutput');
+      managerInterface.RegisterAgent('/bluezplayer/agent', 'DisplayOnly');
       managerInterface.RequestDefaultAgent('/bluezplayer/agent');
       console.log(managerInterface);
       console.log('BluezPlayer is a default agent');
