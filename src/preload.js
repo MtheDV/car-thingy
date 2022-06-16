a3b372e00cfe557413e5ca72b9acc09d38d6b35d
Mathew de Vin <mathewdevin03@gmail.com>
2022-06-16 13:38:03 -0700

    Debugging view swapping

--- src/preload.js
+++ src/preload.js
@@ -25,7 +25,7 @@ contextBridge.exposeInMainWorld('api', {
     ipcRenderer.on('set-position-update', callback);
   },
   onDeviceUpdate: (callback) => {
-    console.log(callback);
+    console.log(callback[0], callback[1]);
     ipcRenderer.on('set-device-update', callback);
   },
   // Handle connecting to devices and pairing new devices
